82133902ab3e0ca0bce71a08f915d097e1e57f67
samaa168 <smaelsyd@gmail.com>
2023-09-20 09:41:35 -0700

    task1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,25 @@
 #include "main.h"
 /**
- * *_strcat - function that concatenates teo strings
+ * _strcat - function that concatenates teo strings
  * @dest: pointer to destnation input
  * @src: pointer to source input
  * Return: pointer to resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
+	int c = 0;
+	int b = 0;
+	int i;
 
-	c = 0;
-	while (dest[c])
+	for (i = 0; dest[i] != '\0'; i++)
 		c++;
 
-	for (c2 = 0; src[c2]; c2++)
-		dest[c++] = src[c2];
+	for (i = 0; src[i] != '\0'; i++)
+		b++;
 
+	for (i = 0; i <= b; i++)
+	{
+		dest[c = i] = src[i];
+	}
 	return (dest);
 }
