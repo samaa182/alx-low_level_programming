ba0d0c8343ff1f7c093079264e2d24e56d914321
samaa168 <smaelsyd@gmail.com>
2023-09-20 10:11:48 -0700

    task1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,13 @@ char *_strcat(char *dest, char *src)
 	int c, b;
 
 	c = 0;
+	/*find the size of dest array*/
 	while (dest[c])
 		c++;
 
+	/*interate through each src array value without the null byte*/
 	for (b = 0; src[b]; b++)
+		/*append src[b] to dest[c] while overwritting the null byte in dest*/
 		dest[c++] = src[b];
 
 	return (dest);
