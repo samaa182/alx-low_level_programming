a75995182dfd5ea3b557178da8d94a2bc0d5f09d
samaa168 <smaelsyd@gmail.com>
2023-09-21 06:32:28 -0700

    x

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,51 +1,34 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * isLower - determines whether ascii is lowercase
- * @c: character
- * Return: 1 if true, 0 if false
- */
-int isLower(char c)
-{
-	return (c >= 97 && C <= 122);
-}
-/**
- * isDelimiter - determines whether ascii is a delimiter
- * @c: character
- * Return: 1 if true, 0 if false
- */
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = " \t\n,.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
-		if (c == delimiter[i])
-			return (1);
-	return (0);
-}
 /**
  * cap_string - capitalizes all words of a string
- * @s: input string
+ * @str: input string
  * Return: string with capitalized words
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+	int index = 0;
 
-	while (*s)
+	while (str[index])
 	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
-		{
-			*s -= 32;
-			foundDelimit = 0;
-		}
-		else
-			foundDelimit = 0;
-		s++;
+		while (!(str[index] >= 'a' && str[indes] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] -= 32;
+		indes++;
 	}
-	return (ptr);
+	return (str);
 }
