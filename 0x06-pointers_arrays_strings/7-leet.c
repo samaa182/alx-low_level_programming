ec3998f9258b29573d5d47dde72fc40a9b09b8ef
samaa168 <smaelsyd@gmail.com>
2023-09-21 07:34:41 -0700

    x

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,11 +8,11 @@ char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
-	int s2[] = "4433007711";
+	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 			{
