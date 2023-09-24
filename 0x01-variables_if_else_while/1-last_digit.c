d5de5aefcb3ad46195c073a3fe81c94aa7244ce5
samaa168 <smaelsyd@gmail.com>
2023-09-24 14:18:01 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,15 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - entry point
  * Description: print the value of n status:
  *	 greater than, is zero and is not less than 6
  * Return: 0
  */
 int main(void)
 {
-	int n;
-	int m;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -20,7 +19,7 @@ int main(void)
 		printf("Last digit of %d is %d  and is greater than 5\n", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	else
+	else if	(m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: point all aplhabet letters
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = "a";
+
+	while (ch <= "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+
+	return (0);
+}
