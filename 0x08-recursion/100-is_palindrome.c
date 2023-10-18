15b883194e33c9334fa5b86ab245731aaa84bef9
samaa168 <smaelsyd@gmail.com>
2023-10-18 07:14:20 -0700

    m

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,3 +26,19 @@ int end = last_indes(s);
 
 return (check(s, 0, end - 1, mod));
 }
+/**
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from left to right
+ * @mod: int
+ * Return: 0 or 1
+ */
+int check(char *s, int start, int end, int mod)
+{
+	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, mod));
+}
